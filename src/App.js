ad78da347e205042cfeb6ba45fc96d275af85ea4
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-14 00:25:52 +0530

    add differnebt routes for different pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
 import SideBar from "./components/sideBar";
+import Home from "./pages/features/Home";
+import Bookmark from "./pages/features/Bookmark";
+import LikedPost from "./pages/features/LikedPost";
+import Explorer from "./pages/features/Explorer";
 
 function App() {
   return (
     <div className="App">
       <SideBar/>
       <Routes>
-        <Route path="/"/>
+        <Route path="/"element={<Home/>}/>
         <Route path ="/signup"element={<Signup/>}/>
         <Route path ="/login"element={<Login/>}/>
+        <Route path="/bookmark"element={<Bookmark/>}/>
+        <Route path="/likedpost"element={<LikedPost/>}/>
+        <Route path="/explore"element={<Explorer/>}/>
       </Routes>
     </div>
   );
--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -4,19 +4,19 @@ export default function SideBar() {
   return (
     <div>
         <div>
-      <Link>
+      <Link to="/">
         <span><i className="fa fa-home"></i></span>
         Home
       </Link>
       </div>
       <div>
-      <Link> <span><i className="fa fa-compass"></i></span>Explorer</Link>
+      <Link to="/explore"> <span><i className="fa fa-compass"></i></span>Explorer</Link>
       </div>
       <div>
-      <Link> <span><i className="fa fa-bookmark"></i></span>Bookmark</Link>
+      <Link to="/bookmark"> <span><i className="fa fa-bookmark"></i></span>Bookmark</Link>
       </div>
       <div>
-      <Link> <span><i className="fa fa-heart"></i></span>Like</Link>
+      <Link to="/likedpost"> <span><i className="fa fa-heart"></i></span>Like</Link>
       </div>
     </div>
   );
