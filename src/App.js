9a29d775e4a7d99a9fe0557a20cf8972ea76d243
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-13 11:43:51 +0530

    add react router and react router dom dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Auth/Signup";
+
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path="/"/>
+        <Route path ="/signup"element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
