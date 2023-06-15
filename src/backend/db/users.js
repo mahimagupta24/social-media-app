a6e05e336a38d0b1995cf73d0dbfa0052f95edca
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-16 01:09:38 +0530

    add some data

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,52 +8,111 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
+    fullname: "Adarsh Balika",
     username: "adarshbalika",
     password: "adarshbalika123",
-    bio:"Social Media Influencer",
+    bio: "Social Media Influencer",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    followers: [
+      { _id: uuid(), fullname: "Shubham Soni", username: "shubhamsoni" },
+      { _id: uuid(), fullname: "Mahima Gupta", username: "mahimagupta" },
+      { _id: uuid(), fullname: "Raima Gupta", username: "raimagupta" },
+    ],
+    following: [
+      { _id: uuid(), fullname: "Mahima Gupta", username: "mahimagupta" },
+    ],
   },
   {
     _id: uuid(),
-    firstName: "Shubham",
-    lastName: "Soni",
+    fullname: "Shubham Soni",
     username: "shubhamsoni",
     password: "shubhamsoni123",
-    bio:"Be yourself",
+    bio: "Be yourself",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    followers: [
+      { _id: uuid(), fullname: "Raima Gupta", username: "raimagupta" },
+    ],
+    following: [
+      { _id: uuid(), fullname: "John Doe", username: "johndoe" },
+      {
+        _id: uuid(),
+        fullname: "Adarsh Balika",
+        username: "adarshbalika",
+      },
+    ],
   },
   {
     _id: uuid(),
-    firstName: "Mahima",
-    lastName: "Gupta",
+    fullname: "Mahima Gupta",
     username: "mahimagupta",
     password: "mahimagupta123",
-    bio:"Aspiring Web developer",
+    bio: "Aspiring Web developer",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    followers: [
+      {
+        _id: uuid(),
+        fullname: "Adarsh Balika",
+        username: "adarshbalika",
+      },
+      { _id: uuid(), fullname: "Shubham Soni", username: "shubhamsoni" },
+    ],
+    following: [
+      { _id: uuid(), fullname: "Adarsh Balika", username: "adarshbalika" },
+    ],
   },
   {
     _id: uuid(),
-    firstName: "Raima",
-    lastName: "Gupta",
+    fullname: "Raima Gupta",
     username: "raimagupta",
     password: "raimagupta123",
-    bio:"Frontend Engineer",
+    bio: "Frontend Engineer",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    followers: [
+      {
+        _id: uuid(),
+        fullname: "John Doe",
+        username: "johndoe",
+      },
+    ],
+    following: [
+      { _id: uuid(), fullname: "Shubham Soni", username: "shubhamsoni" },
+      { _id: uuid(), fullname: "Adarsh Balika", username: "adarshbalika" },
+      {
+        _id: uuid(),
+        fullname: "John Doe",
+        username: "johndoe",
+      },
+    ],
   },
   {
     _id: uuid(),
-    firstName: "John",
-    lastName: "Doe",
+    fullname: "John Doe",
     username: "johndoe",
     password: "johndoe123",
-    bio:"What's up?",
+    bio: "What's up?",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    follower: [
+      {
+        _id: uuid(),
+        fullname: "Shubham Soni",
+        username: "shubhamsoni",
+      },
+      {
+        _id: uuid(),
+        fullname: "Raima Gupta",
+        username: "raimagupta",}
+    ],
+    following: [
+      {
+        _id: uuid(),
+        fullname: "Raima Gupta",
+        username: "raimagupta",
+      },
+    ],
   },
 ];
