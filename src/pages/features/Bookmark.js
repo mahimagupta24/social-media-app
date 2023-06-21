5ea474ba9e7bae1c5ff0f0a7e268511b73733424
mahimagupta24 <mahimagupta2401@gmail.com>
2023-06-22 01:25:55 +0530

    add bookmark functionality

--- src/pages/features/Bookmark.js
+++ src/pages/features/Bookmark.js
@@ -3,15 +3,14 @@ import { UserContext } from "../../contexts/UserContext";
 
 export default function Bookmark() {
   const { state } = useContext(UserContext);
-  console.log(state.bookmarkPosts);
+  // console.log(state.bookmarkPosts);
 
   return (
     <div>
-      {state?.bookmarkPosts?.map(({ _id,firstName, lastName,content }) => (
+      {state?.bookmarkPosts?.map(({ _id, firstName, lastName, content }) => (
         <li key={_id}>
           <p>
             {firstName} {lastName}
-            
           </p>
           <p>{content}</p>
         </li>
